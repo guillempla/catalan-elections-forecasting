f215ad749a0f4139c3ee466d39158404d307286c
Guillem Pla <guillempla@protonmail.com>
2024-01-23 22:27:23 +0100

    Create CleanData class and add typing to functions

--- src/clean-data/app.py
+++ src/clean-data/app.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 import logging
 import pandas as pd
 
@@ -8,39 +10,31 @@ logging.basicConfig(
 )
 
 
-def load_catalan_elections_data():
+def load_csv(filename: str) -> pd.DataFrame:
     """
     Load data from CSV file.
     """
     logging.info("Loading data from CSV.")
-    return pd.read_csv("data/raw/catalan-elections-data.csv")
+    return pd.read_csv(filename)
 
 
-def load_election_days_data():
-    """
-    Load data from CSV file.
-    """
-    logging.info("Loading data from CSV.")
-    return pd.read_csv("data/processed/election-days.csv")
-
-
-def drop_columns(df, columns_to_drop: list):
+def drop_columns(df: pd.DataFrame, columns_to_drop: List[str]) -> pd.DataFrame:
     """
     Drop columns from dataframe.
     """
-    logging.info("Dropping columns: " + columns_to_drop)
+    logging.info("Dropping columns: %s", columns_to_drop)
     return df.drop(columns=columns_to_drop)
 
 
-def rename_columns(df, columns_to_rename: dict):
+def rename_columns(df: pd.DataFrame, columns_to_rename: Dict[str, str]) -> pd.DataFrame:
     """
     Rename columns from dataframe.
     """
-    logging.info("Renaming columns: " + columns_to_rename)
+    logging.info("Renaming columns: %s", columns_to_rename)
     return df.rename(columns=columns_to_rename)
 
 
-def divide_id_eleccio(df):
+def divide_id_eleccio(df: pd.DataFrame) -> pd.DataFrame:
     """
     Divide id_eleccio column into id_partit and id_circunscripcio.
     """
@@ -51,41 +45,83 @@ def divide_id_eleccio(df):
     return df
 
 
-def merge_election_days(df_elections_data, df_elections_days):
+def merge_election_days(
+    df: pd.DataFrame, df_elections_days: pd.DataFrame
+) -> pd.DataFrame:
     """
     Merge election_days dataframe into df.
     """
     logging.info("Merging election_days dataframe.")
-    return df_elections_data.merge(
-        df_elections_days, on=["nom_eleccio", "year"], how="left"
-    )
+    return df.merge(df_elections_days, on=["nom_eleccio", "year"], how="left")
 
 
-def filter_by_election_type(df, election_type: list):
+def filter_by_election_type(
+    df: pd.DataFrame, elections_type: List[str]
+) -> pd.DataFrame:
     """
     Filter dataframe by election type.
     """
     logging.info("Filtering by election type.")
 
     # Check if election_type is a list
-    if not isinstance(election_type, list):
+    if not isinstance(elections_type, list):
         raise TypeError("election_type must be a list.")
 
     # Check if election_type is empty
-    if not election_type:
+    if not elections_type:
         raise ValueError("election_type cannot be empty.")
 
     # Check if election_type contains valid values
-    valid_election_type = ["A", "C", "D", "E", "G", "M", "S", "V"]
-    if not all(elem in valid_election_type for elem in election_type):
+    valid_election_type = df["type"].unique()
+    if not all(elem in valid_election_type for elem in elections_type):
         raise ValueError("election_type contains invalid values.")
 
-    return df[df["type"].isin(election_type)]
+    return df[df["type"].isin(elections_type)]
 
 
-def save_data(df, filename: str):
+def save_data(df: pd.DataFrame, filename: str):
     """
     Save dataframe as CSV file.
     """
     logging.info("Saving data as CSV.")
     df.to_csv(filename, index=False)
+
+
+class CleanData:
+    """
+    Clean data.
+    """
+
+    def __init__(
+        self,
+        elections_data_filename: str = "data/raw/catalan-elections-data.csv",
+        elections_days_filename: str = "data/processed/election-days.csv",
+        output_filename: str = "data/processed/catalan-elections-clean-data.csv",
+        elections_type: List[str] = [
+            "M",
+            "E",
+            "A",
+            "G",
+        ],  # M: Municipals, E: Europees, A: Autonòmiques, G: Generals
+    ) -> None:
+        """
+        Initialize class.
+        """
+        self.df = load_csv(elections_data_filename)
+        self.elections_days_df = load_csv(elections_days_filename)
+        self.output_filename = output_filename
+        self.elections_type = elections_type
+
+    def clean_elections_data(self):
+        """
+        Clean elections data.
+        """
+        logging.info("Cleaning elections data.")
+        self.df = (
+            self.df.pipe(drop_columns, columns_to_drop=["candidat_posicio"])
+            .pipe(rename_columns, columns_to_rename={"secci_": "seccio"})
+            .pipe(divide_id_eleccio)
+            .pipe(filter_by_election_type, elections_type=self.elections_type)
+            .pipe(merge_election_days, df_elections_days=self.elections_days_df)
+            .pipe(save_data, filename=self.output_filename)
+        )

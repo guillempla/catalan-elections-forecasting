ee8166d8f3ff615b68d7c6575d04d6e0dc49b622
Guillem Pla <guillempla@protonmail.com>
2024-02-24 17:37:52 +0100

    Adapt to download multiple socrata datasets

--- src/download_data/download_data.py
+++ src/download_data/download_data.py
@@ -1,17 +1,15 @@
 """
-Module for downloading data from a Socrata dataset.
+Module for downloading data.
 """
 
 import logging
-from .catalan_elections_data import DownloadCatalanElectionsData
-import logging
-from typing import Optional
+from typing import List
 from .catalan_elections_data import DownloadCatalanElectionsData
 
 
 class DownloadData:
     """
-    Class for downloading data from a Socrata dataset.
+    Class for downloading data.
 
     Args:
         socrata_domain (str): The domain of the Socrata API.
@@ -27,40 +25,34 @@ class DownloadData:
 
     def __init__(
         self,
-        socrata_domain: str,
-        socrata_app_token: str,
-        socrata_username: str,
-        socrata_password: str,
-        socrata_dataset_id: str,
-        csv_path: Optional[str] = None,
-        pkl_path: Optional[str] = None,
+        dataset_configs: List[dict],
     ) -> None:
         """
         Initializes a new instance of the DownloadData class.
 
         Args:
-            socrata_domain (str): The domain of the Socrata API.
-            socrata_app_token (str): The app token for accessing the Socrata API.
-            socrata_username (str): The username for authentication.
-            socrata_password (str): The password for authentication.
-            socrata_dataset_id (str): The ID of the Socrata dataset.
-            csv_path (str, optional): The path to save the downloaded data as a CSV file. Defaults to None.
-            pkl_path (str, optional): The path to save the downloaded data as a pickle file. Defaults to None.
+            dataset_configs (List[dict]): A list of dictionaries containing the configuration.
         """
         logging.info("Starting data download.")
 
-        self.catalan_elections_data_downloader = DownloadCatalanElectionsData(
-            socrata_domain,
-            socrata_app_token,
-            socrata_username,
-            socrata_password,
-            socrata_dataset_id,
-            csv_path,
-            pkl_path,
-        )
+        self.catalan_elections_data_downloaders: List[DownloadCatalanElectionsData] = []
+        for dataset_config in dataset_configs:
+            self.catalan_elections_data_downloaders.append(
+                DownloadCatalanElectionsData(
+                    dataset_config["socrata_domain"],
+                    dataset_config["socrata_app_token"],
+                    dataset_config["socrata_username"],
+                    dataset_config["socrata_password"],
+                    dataset_config["socrata_dataset_id"],
+                    dataset_config["csv_path"],
+                    dataset_config["pkl_path"],
+                )
+            )
 
     def download_catalan_elections_data(self) -> None:
         """
         Downloads the Catalan elections data using the configured parameters.
         """
-        self.catalan_elections_data_downloader.download_catalan_elections_data()
+
+        for downloader in self.catalan_elections_data_downloaders:
+            downloader.download_catalan_elections_data()
